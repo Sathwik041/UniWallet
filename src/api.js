302988b165d9ec61cce7cd0b649b718f8c94fd29
Sathwik Annaldesh <93048377+Sathwik041@users.noreply.github.com>
2026-02-04 10:38:37 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://wallet-genertor-backend.vercel.app',
+  baseURL: import.meta.env.VITE_API_URL || 'https://wallet-generator-backend.vercel.app',
 });
 
 export default api;
